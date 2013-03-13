b31d8e20c73d6d55dc047de6bc6190422e6bdf7a
Akshay Khole <akshay@reversehack.in>
2013-03-14 00:09:17 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yaeo::Application.config.secret_token = '9efa0e4b74aa390d5adb01d84c29d9b3a5037f713e7a01007914432f3b59a3705aa7e13afb269ab60849384a6da20246e2dee213ec9f3f20f661d49e19eb12f8'
