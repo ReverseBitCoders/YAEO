10f61f2f34a41ccf17c6691699fed43597fb162a
Akshay Khole <akshay@reversehack.in>
2013-03-17 00:00:56 +0530

    added basic properties to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event
   include DataMapper::Resource
 
   property :id, Serial
-
+  property :name, String, :length => 100, :required => true
+  property :description, Text, :required => false
+  property :date_time, DateTime, :required => false
 
 end
