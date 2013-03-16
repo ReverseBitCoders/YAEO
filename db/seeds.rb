baa333b175334cd7566ca0b6103c226d118ba366
Akshay Khole <akshay@reversehack.in>
2013-03-17 01:34:37 +0530

    db updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+ { :name => 'Test event one', :date_time => DateTime.today.utc}
+ { :name => 'Test event two', :date_time => DateTime.today.utc}
+ { :name => 'Test event three', :date_time => DateTime.today.utc}
+].each do |event|
+  Event.create(event)
+end
