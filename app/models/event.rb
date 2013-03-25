569d9752bc434f320e6b3a7a79898cb9c0419ee1
Akshay Khole <akshay@reversehack.in>
2013-03-25 19:09:04 +0530

    adding created_by user id in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,6 @@ class Event
   property :name, String, :length => 100, :required => true
   property :description, Text, :required => false
   property :date_time, DateTime, :required => false
-
+  property :created_by_user_id, Integer, :required => true
   # has n, :address
 end
