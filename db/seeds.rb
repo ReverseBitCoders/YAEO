8d2789bef8aaac7ce6583c456754681b08528b7a
Akshay Khole <akshay@reversehack.in>
2013-05-05 15:39:24 +0530

    fixing minor stuff.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 [
- {:user_id => 1, :name => 'Clojure meetup', :description => 'All clojure geeks unite ! ', :date_time => DateTime.now.utc },
- {:user_id => 1, :name => 'JUG', :description => 'Java users meetup group. Meets every thursday and do boring stuff ', :date_time => DateTime.now.utc},
- {:user_id => 1, :name => 'Pune linux users group', :description => 'Prestigious community of pune linux users meet up and contri and eat bhel puri ', :date_time => DateTime.now.utc}
+ {:user_id => 1, :name => 'event1', :description => 'event 1 description ', :date_time => DateTime.now.utc },
 ].each do |event|
   Event.create(event)
 end
