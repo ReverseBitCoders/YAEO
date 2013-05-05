6edd74fd822e69e23f23cfa12f262f3c7d6d9fd5
Akshay Khole <akshay@reversehack.in>
2013-05-05 13:03:12 +0530

    moving some paths into helper methods

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,15 @@
 module EventsHelper
+
+  def events_top_header_path
+    'events/top_header'
+  end
+
+  def events_left_pane_path
+    'events/top_header'
+  end
+
+  def events_right_pane_path
+    'events/right_pane'
+  end
+
 end
