176d032fbf439edfd5a220e12ba5d655ac51a2ae
Akshay Khole <akshay@reversehack.in>
2013-05-05 12:57:00 +0530

    clean up some code in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,6 @@ class EventsController < ApplicationController
     @events = Event.all
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @events }
     end
   end
 
@@ -17,7 +16,6 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @event }
     end
   end
 
@@ -28,7 +26,6 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @event }
     end
   end
 
@@ -45,10 +42,8 @@ class EventsController < ApplicationController
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render json: @event, status: :created, location: @event }
       else
         format.html { render action: "new" }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -64,20 +59,19 @@ class EventsController < ApplicationController
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
-  def destroy
-    @event = Event.get(params[:id])
-    @event.destroy
+  # # DELETE /events/1
+  # # DELETE /events/1.json
+  # def destroy
+  #   @event = Event.get(params[:id])
+  #   @event.destroy
 
-    respond_to do |format|
-      format.html { redirect_to events_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to events_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 end
